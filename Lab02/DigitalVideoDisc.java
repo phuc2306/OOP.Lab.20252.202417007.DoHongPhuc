7acb28946375277c371ecfd73e7f75ee0abe6844
Do Hong Phuc <babyanh940@gmail.com>
2026-03-31 21:16:50 +0700

    bai tap lab2

---
+++ Lab02/DigitalVideoDisc.java
@@ -0,0 +1,22 @@
+package AimsProject;
+
+public class DigitalVideoDisc {
+	private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+}
