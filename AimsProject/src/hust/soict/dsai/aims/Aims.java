32de197cb76167b852d876944dd8e62bbdab09a3
Do Hong Phuc <phuc.dh2417007@sis.hust.edu.vn>
2026-04-22 22:07:16 +0700

    Save local AimsProject before merging

---
+++ AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -0,0 +1,19 @@
+package hust.soict.dsai.aims;
+
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class Aims {
+    public static void main(String[] args) {
+        // Tạo giỏ hàng
+        Cart anOrder = new Cart();
+
+        // Tạo các đĩa DVD
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 124, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "John Musker", 90, 18.99f);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("The Matrix", "Action", "Wachowski", 136, 20.00f);
+
+
+
+}}
---
+++ AimsProject/src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
@@ -0,0 +1,53 @@
+package hust.soict.dsai.aims.disc;
+
+public class DigitalVideoDisc {
+	private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    private int id; 
+    private static int nbDigitalVideoDiscs = 0; 
+
+    private void updateTotalAndId() {
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+        updateTotalAndId();
+    }
+
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        updateTotalAndId();
+    }
+
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+        updateTotalAndId();
+    }
+
+
+    public String getTitle() { return title; }
+    public String getCategory() { return category; }
+    public String getDirector() { return director; }
+    public int getLength() { return length; }
+    public float getCost() { return cost; }
+    public int getId() { return id; }
+    
+    public void setTitle(String title) { this.title = title; }
+    public String toString() { {
+        return "DVD - " + title + " - " + category + " - " + director + " - " + length + ": " + cost + " $";
+    }}
+}
