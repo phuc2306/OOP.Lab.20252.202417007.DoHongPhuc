bb84afb48c340b1bd81e0eedd526445db647e39b
phuc2306 <babyanh940@gmail.com>
2026-04-22 17:41:41 +0700

    Delete OtherProjects directory

--- OtherProjects/hust/soict/dsai/lab01/bai221.java
+++
@@ -1,7 +0,0 @@
-package hust.soict.dsai.lab01;
-public class bai221 {
-    public static void main(String[] args){
-        System.out.printf("Xin chào các bạn\n");
-        System.out.printf("Hello world\n");
-    }
-}
--- OtherProjects/hust/soict/dsai/lab01/bai222.java
+++
@@ -1,8 +0,0 @@
-package hust.soict.dsai.lab01;
-import javax.swing.JOptionPane;
-
-public class bai222 {
-    public static void main(String[] args){
-                JOptionPane.showMessageDialog(null, "Hello world! How are you?");
-    }
-}
