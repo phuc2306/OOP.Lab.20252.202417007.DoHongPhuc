d9db03c6065e14b8d388c8a52655169aa941116d
phuc2306 <babyanh940@gmail.com>
2026-04-22 19:37:55 +0700

    Add files via upload

--- Aims.java
+++ Aims.java
@@ -1,7 +1,5 @@
-package hust.soict.dsai.aims;
+package AimsProject;
 
-import hust.soict.dsai.aims.cart.Cart;
-import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 
 public class Aims {
     public static void main(String[] args) {
@@ -14,13 +12,6 @@ public class Aims {
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "John Musker", 90, 18.99f);
         DigitalVideoDisc dvd4 = new DigitalVideoDisc("The Matrix", "Action", "Wachowski", 136, 20.00f);
 
-        // Test 1: Thêm 2 đĩa cùng lúc
-        System.out.println("--- TEST: ADD 2 DVDS ---");
-        anOrder.addDigitalVideoDisc(dvd1, dvd2);
 
-        // Test 2: Thêm 1 mảng đĩa
-        System.out.println("\n--- TEST: ADD AN ARRAY OF DVDS ---");
-        DigitalVideoDisc[] dvdList = {dvd3, dvd4};
-        anOrder.addDigitalVideoDisc(dvdList);
-    }
-}
+System.out.println("Total DVDs created: " + DigitalVideoDisc.getNbDigitalVideoDiscs());
+}}
