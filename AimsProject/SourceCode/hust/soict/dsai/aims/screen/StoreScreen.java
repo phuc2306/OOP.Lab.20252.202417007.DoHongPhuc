84a110e148b8ed907a8c9e548a3fad63c44a98eb
Do Hong Phuc <phuc.dh2417007@sis.hust.edu.vn>
2026-06-01 23:12:27 +0700

    Hoan thanh phan 6: Thiet ke giao dien StoreScreen bang Swing

--- AimsProject/SourceCode/hust/soict/dsai/aims/screen/StoreScreen.java
+++ AimsProject/SourceCode/hust/soict/dsai/aims/screen/StoreScreen.java
@@ -1,27 +1,42 @@
 package hust.soict.dsai.aims.screen;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.util.ArrayList;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import hust.soict.dsai.aims.store.Store;
-import hust.soict.dsai.aims.media.*;
+import hust.soict.dsai.aims.media.Media;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
 
 public class StoreScreen extends JFrame {
     private Store store;
-
+    
     public StoreScreen(Store store) {
         this.store = store;
         Container cp = getContentPane();
         cp.setLayout(new BorderLayout());
-
+        
         cp.add(createNorth(), BorderLayout.NORTH);
         cp.add(createCenter(), BorderLayout.CENTER);
-
+        
         setVisible(true);
         setTitle("Store");
         setSize(1024, 768);
     }
-
+    
     JPanel createNorth() {
         JPanel north = new JPanel();
         north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
@@ -29,7 +44,7 @@ public class StoreScreen extends JFrame {
         north.add(createHeader());
         return north;
     }
-
+    
     JMenuBar createMenuBar() {
         JMenu menu = new JMenu("Options");
         
@@ -48,7 +63,7 @@ public class StoreScreen extends JFrame {
         
         return menuBar;
     }
-
+    
     JPanel createHeader() {
         JPanel header = new JPanel();
         header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
@@ -69,31 +84,25 @@ public class StoreScreen extends JFrame {
         
         return header;
     }
-
+    
     JPanel createCenter() {
         JPanel center = new JPanel();
         center.setLayout(new GridLayout(3, 3, 2, 2));
         
         ArrayList<Media> mediaInStore = store.getItemsInStore();
-        for (int i = 0; i < 9; i++) {
-            if (i < mediaInStore.size()) {
-                MediaStore cell = new MediaStore(mediaInStore.get(i));
-                center.add(cell);
-            } else {
-                center.add(new JPanel()); // them o trong neu khong du 9 items
-            }
+        for (int i = 0; i < mediaInStore.size(); i++) {
+            MediaStore cell = new MediaStore(mediaInStore.get(i));
+            center.add(cell);
         }
         
         return center;
     }
-
+    
     public static void main(String[] args) {
         Store store = new Store();
-        // them vai san pham mau de chay thu
         store.addMedia(new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f));
         store.addMedia(new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f));
         store.addMedia(new DigitalVideoDisc("Aladdin", "Animation", 18.99f));
-        
         new StoreScreen(store);
     }
 }
